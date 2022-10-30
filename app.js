02e1d266a09abc7e334b835af94b2ada7ab43ca3
NikolovToni <nikolov.s.toni@gmail.com>
2022-10-30 14:19:16 +0200

    Making the functionality of the add button for the ul list.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
@@ -18,9 +21,17 @@ app.get("/", function(req, res){
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     });
 
+});
+
+app.post("/", function(req, res){
+    item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
 });
 
 
