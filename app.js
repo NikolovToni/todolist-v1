6db31905f318eb698269b011d13e8fe681f8c2fc
NikolovToni <nikolov.s.toni@gmail.com>
2022-10-31 13:04:22 +0200

    Adding .ejs header and footer.

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
     let today = new Date();
 
@@ -22,23 +23,38 @@ app.get("/", function(req, res){
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 
 });
 
-app.post("/", function(req, res){
-    item = req.body.newItem;
-    items.push(item);
+app.post("/", function (req, res) {
 
-    res.redirect("/");
+    item = req.body.newItem;
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    req.redirect("/work");
 });
 
 
 
-
-
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 });
